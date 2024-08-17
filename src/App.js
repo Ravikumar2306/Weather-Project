1cae97d8884c1cedd074a2deaf3c592c2899057a
seetharam <ravivijay2306@gmail.com>
2024-08-17 18:29:13 +0530

    changes updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import InputArea from './components/inputArea';
-import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Routes>
-        {/* Add routes here if you have other components */}
-        {/* Example route */}
-        {/* <Route path="/" element={<YourExistingComponent />} /> */}
-      </Routes>
-      <InputArea /> {/* Component to be rendered */}
+      <InputArea />
     </div>
   );
 }

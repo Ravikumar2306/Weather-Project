5b20b3ad9a668f113cd408bca8b4202515f15b12
Ravikumar2306 <145085862+Ravikumar2306@users.noreply.github.com>
2024-08-17 18:23:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import InputArea from './components/inputArea';
+import InputArea from '/components/inputArea';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
